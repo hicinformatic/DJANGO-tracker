6eea33abace45813ec25ab3d0cfc2341247d1904
hicinformatic <hicinformatic@gmail.com>
2017-04-13 21:16:50 +0200

    test

--- templates/tracker/tracker.js
+++ templates/tracker/tracker.js
@@ -6,6 +6,7 @@ visit.visit = function() {
     this.add('url', this.url);
     this.add('title', this.document.title);
     params = params.join('&');
+    alert(params);
     var xhr = new XMLHttpRequest();
     xhr.open("POST", '{{ url }}/visit.html/{{ domain }}/', true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
