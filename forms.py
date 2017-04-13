df077210d86e2a9fb55fb6bd6a3d985cbc553085
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 17:51:48 +0200

    test

--- forms.py
+++ forms.py
@@ -3,12 +3,12 @@ from django import forms
 from .settings import conf
 try:
     from .moreconf import more
-    conf['data'] = conf['data']+more
+    DatasAuthorized = conf['datas']+more
 except Exception:
-    pass
+    DatasAuthorized = conf['datas']
 
 class trackFormDatas(forms.Form):
     def __init__(self, *args, **kwargs):
         super(trackFormDatas, self).__init__(*args, **kwargs)
-        for field in conf['datas']:
+        for field in DatasAuthorized:
             self.fields[field] = forms.CharField(required=False)
