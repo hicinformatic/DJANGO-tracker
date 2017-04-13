d301b169477c51513096cb62dce0edd295d9b0df
hicinformatic <hicinformatic@gmail.com>
2017-04-13 23:23:55 +0200

    test

--- templates/tracker/tracker.js
+++ templates/tracker/tracker.js
@@ -1,5 +1,6 @@
-function visit() {
+function visit(url=null) {
     this.params = [];
+    this.url = url;
     this.add = function(key, value) {
         this.params.push(key + '=' + value);
     };
@@ -8,11 +9,21 @@ function visit() {
         this.add('title', document.title);
         params = this.params.join('&');
         var xhr = new XMLHttpRequest();
-        xhr.open("POST", '{{ url }}/visit.html/{{ domain }}/', true);
+        xhr.open("POST", url + '/visit.html/{{ domain }}/', true);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.send(params); 
         this.params = [];
     };
+    this.start function() {
+        if( this.url == null ) {
+            alert('null');
+            this.url = ''
+        }else{
+            alert('pas null');
+            this.url = '//' + this.url
+        }
+    }
+    this.start();
 }
 visit = new visit();
 visit.add('height', window.screen.height);
