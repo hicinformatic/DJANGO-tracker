f3567f9c058f70acdb73f6dace372aa68bc39cfa
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 18:36:38 +0200

    directJS

--- urls.py
+++ urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^visit.svg/(?P<visitor>.{1,32})/$', views.trackerSVG, name='trackerSVG'),
     url(r'^visit.html$', views.trackerDATAS, name='trackerDATAS'),
     url(r'^visit.html/(?P<domain>.{1,32})/$', views.trackerDATAS, name='trackerDATAS'),
-    url(r'^visit.js/(?P<domain>.{1,32})/$', views.insertJS, name='insertJS'),
+    url(r'^visit.js/(?P<domain>.{1,32})/$', views.directJS, name='directJS'),
     url(r'^downlad.js/(?P<domain>.{1,32})/$', views.downloadJS, name='downloadJS'),
 ]
