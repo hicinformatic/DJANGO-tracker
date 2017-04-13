55eb7d9056c9900503d31643686907d94a6ac985
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 16:11:18 +0200

    test

--- admin.py
+++ admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from .models import Domain, DataAuthorized, Tracked
 
 @admin.register(Domain)
 class DomainAdmin(admin.ModelAdmin):
-    list_display = ( 'domain', 'id', 'status',  )
+    list_display = ( 'domain', 'id', 'status', 'counter', )
     readonly_fields = ( 'domain', 'id', 'create', 'update', )
 
+
+def loadDatasAuthorized(modeladmin, request, queryset):
+    self.message_user(request, _('Authorized data loaded'), 'success')
+loadDatasAuthorized.short_description = _('Loads authorized data')
 @admin.register(DataAuthorized)
 class DataAuthorizedAdmin(admin.ModelAdmin):
-    list_display = ( 'key', 'status', )
-    readonly_fields = ( 'create', 'update', )
+    list_display = ( 'key', 'status', 'counter', )
+    readonly_fields = ( 'create', 'update', 'counter', )
+    actions = [ loadDatasAuthorized, ]
 
 @admin.register(Tracked)
 class TrackedAdmin(admin.ModelAdmin):
