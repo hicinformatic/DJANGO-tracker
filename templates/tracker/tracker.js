20cc238f72d0e4fdc1859456a6ad4b09d3b75706
hicinformatic <hicinformatic@gmail.com>
2017-04-13 21:41:38 +0200

    test

--- templates/tracker/tracker.js
+++ templates/tracker/tracker.js
@@ -1,16 +1,19 @@
-var visit = visit || {};
-visit.params = [];
-visit.add = function(key, value) { this.params.push(key + '=' + value); }
-visit.visit = function() {
-    alert('start');
-    this.add('url', window.location.href);
-    this.add('title', document.title);
-    params = params.join('&');
-    alert(params);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", '{{ url }}/visit.html/{{ domain }}/', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(params); 
+function visit() {
     this.params = [];
-    alert('end');
+    this.add = function(key, value) {
+        this.params.push(key + '=' + value);
+    };
+    this.visit = function() {
+        alert('start');
+        this.add('url', window.location.href);
+        this.add('title', document.title);
+        params = params.join('&');
+        alert(params);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '{{ url }}/visit.html/{{ domain }}/', true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(params); 
+        this.params = [];
+        alert('end');
+    };
 }
