ffb959dba940c1f42ee670091815b770c087ff39
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 18:19:00 +0200

    test

--- urls.py
+++ urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^visit.svg/(?P<visitor>.{1,32})/$', views.trackerSVG, name='trackerSVG'),
     url(r'^visit.html$', views.trackerDATAS, name='trackerDATAS'),
     url(r'^visit.html/(?P<domain>.{1,32})/$', views.trackerDATAS, name='trackerDATAS'),
-    url(r'^visit.js/(?P<domain>.{1,32})/$', views.downloadJS, name='downloadJS'),)
+    url(r'^visit.js/(?P<domain>.{1,32})/$', views.downloadJS, name='downloadJS'),
 ]
