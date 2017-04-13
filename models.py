9a0f648eee1a14a3d284d6938723a57c4f1ba0ef
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 17:20:08 +0200

    test

--- models.py
+++ models.py
@@ -4,10 +4,11 @@ from django.db import models
 from django.utils.translation import ugettext as _
 
 class Domain(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('Unique ID'), help_text=_('visit.id = ') + self.id, )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_('Unique ID'),)
     domain = models.URLField(verbose_name=_('Domain authorized'),)
     status = models.BooleanField(default=True, verbose_name=_('Enable'),)
     counter = models.BigIntegerField(default=0, verbose_name=_('Counter'),)
+    javascript = models.TextField(blank=True, null=True, editable=False, verbose_name=_('Javascript integration'))
     create = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Creation date'),)
     update = models.DateTimeField(auto_now=True, editable=False, verbose_name=_('Update date'),)
 
@@ -17,6 +18,11 @@ class Domain(models.Model):
 
     def __str__(self):
         return self.domain
+
+    def save(self, *args, **kwargs):
+        self.javascript = 'visit.id = %s;' % self.id.hex
+        super(Authenta, self).save(*args, **kwargs)
+    
 
 class DataAuthorized(models.Model):
     key = models.CharField(max_length=254, unique=True, verbose_name=_('Data'),)
