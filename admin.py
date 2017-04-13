b98cc3eb85d782bbe014137ceb52eb3142b77850
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 14:50:18 +0200

    test

--- admin.py
+++ admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 
 from .models import Domain, DataAuthorized, Tracked
 
-class DataAuthorizedInline(admin.TabularInline):
-    model = DataAuthorized
-    readonly_fields = ( 'key', 'create', 'update', )
-    extra = 0
+
 @admin.register(Domain)
 class DomainAdmin(admin.ModelAdmin):
     list_display = ( 'visitor', 'key', 'value', 'domain', 'create', 'update', )
     readonly_fields = ( 'visitor', 'key', 'value', 'domain', 'create', 'update', )
-    inlines = [ DataAuthorizedInline, ]
+
+@admin.register(DataAuthorized)
+class DataAuthorizedInline(admin.TabularInline):
+    list_display = ( 'key', 'create', 'update', )
+    readonly_fields = ( 'key', 'create', 'update', )
 
 @admin.register(Tracked)
 class TrackedAdmin(admin.ModelAdmin):
