54b3c5f2219a252b1ca6f6f289200fe6f1f0d2c0
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-13 14:47:17 +0200

    test

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from . import views
 from .settings import conf
 
 urlpatterns = [
-    url(r'^'+ conf['url'] +'visit.svg$', views.trackerSVG, name='trackerSVG'),
-    url(r'^'+ conf['url'] +'visit.svg/(?P<visitor>.{1,32})/$', views.trackerSVG, name='trackerSVG'),
-    url(r'^'+ conf['url'] +'visit.html$', views.trackerDATAS, name='trackerDATAS'),
-    url(r'^'+ conf['url'] +'visit.html/(?P<domain>.{1,32})/$', views.trackerDATAS, name='trackerDATAS'),
+    url(r'^visit.svg$', views.trackerSVG, name='trackerSVG'),
+    url(r'^visit.svg/(?P<visitor>.{1,32})/$', views.trackerSVG, name='trackerSVG'),
+    url(r'^visit.html$', views.trackerDATAS, name='trackerDATAS'),
+    url(r'^visit.html/(?P<domain>.{1,32})/$', views.trackerDATAS, name='trackerDATAS'),
 ]
