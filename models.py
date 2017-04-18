ac4f9a8bc25a4e0a8bbd9c69b3e2e15736bf3867
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-04-18 18:02:03 +0200

    validators

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
 from .settings import conf
