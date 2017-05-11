784692ca25d73f29e13c51023e99df2054dc7089
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-11 13:09:25 +0200

    test

--- functions.py
+++ functions.py
@@ -258,10 +258,10 @@ def addTRK_sort_recurring(contenttype, task, script):
     logger = logging.getLogger(__name__)
     for i, v in enumerate(visit):
         if v in existing: 
-            logger.error('del: %S'%v)
+            logger.error('del: %s'%v)
             del visit[i]
         else:
-            logger.error('not del: %S'%v)
+            logger.error('not del: %s'%v)
     #Visitor.objects.bulk_create(visitors)
     return responseOK(contenttype, task, str(visit) + "existing: " + str(existing))
 
