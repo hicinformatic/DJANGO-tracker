a00528fccecdc31ddd5d13a06d71438aed988d5c
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-11 14:29:12 +0200

    test

--- functions.py
+++ functions.py
@@ -238,17 +238,14 @@ def addVisitors(contenttype, task, script):
         visitorsJSON = '{}/{}_visitors.json'.format(conf['taskdir'], script)
         with open(visitorsJSON) as json_data:
             visitors = []
-            listtest = []
             domains = json.load(json_data)
             for domain in domains:
                 try:
                     domobj = Domain.objects.get(id=domain)
-                    for k,v in domains[domain].items():
-                        visitors.append(Visitor(visitor=k, domain=domobj))
+                    for k,v in domains[domain].items(): visitors.append(Visitor(visitor=k, domain=domobj))
                 except Domain.DoesNotExist:
-                    for k,v in domains[domain].items():
-                        visitors.append(Visitor(visitor=k)) 
-            Visitor.objects.bulk_create([v for v in visitors if v.visitor not in [e for e in Visitor.objects.filter(visitor__in=visitors).values_list('visitor', flat=True)] ])
+                    for k,v in domains[domain].items(): visitors.append(Visitor(visitor=k)) 
+            Visitor.objects.bulk_create([v for v in visitors if v.visitor not in [e for e in Visitor.objects.filter(visitor__in=visitors).values_list('visitor', flat=True)]])
     except Exception as e:
         return str(e)
     return True
