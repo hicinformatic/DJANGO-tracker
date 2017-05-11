ff20afe9e6a6efa79a800d3ea865502b75cc8788
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-11 14:21:23 +0200

    test

--- functions.py
+++ functions.py
@@ -235,7 +235,6 @@ def subtask(contenttype, task, secondtask):
 
     try: secondtaskname = conf['subtasks'][script][int(secondtask)]
     except Exception: return responseKO(contenttype, task, 404, _('Subtask not found'))
-
-    #getattr(sys.modules[__name__], secondtaskname)(contenttype, task, script)
+    getattr(subtaskStore, secondtaskname)(contenttype, task, script)
     return responseKO(contenttype, task, 404, _('Task or subtask unavailable'))
 
