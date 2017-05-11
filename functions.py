98d3ffa07b575139665f2d786d43817e51d8b828
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-11 12:38:12 +0200

    test

--- functions.py
+++ functions.py
@@ -252,7 +252,7 @@ def addTRK_sort_recurring(contenttype, task, script):
         return responseKO(contenttype, task, 404, str(e))
     existing = Visitor.objects.filter(visitor__in=visitors).values_list("visitor", flat=True)
     for i, v in enumerate(visitors):
-        if v.visitor in existing: del visitors[idx]
+        if v.visitor in existing: del visitors[i]
     return responseOK(contenttype, task, str(visitors))
 
 def addTask(contenttype, task):
