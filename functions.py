10b6cfbece659f6ffaf93f1f32e00c90d8fc12a0
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-11 16:49:20 +0200

    test

--- functions.py
+++ functions.py
@@ -266,7 +266,7 @@ def addAllInfos(contenttype, task, script):
         with open(datasJSON) as json_data:
             datas = json.load(json_data)
             visitors = Visitor.objects.filter(visitor__in=datas['visitors'])
-            for k,v in datas['useragents']:
+            for k,v in datas['useragents'].items():
                 useragents.append(UserAgentAssociated(visitor=visitors[k], useragent=v['data'], create=v['date']))
     except Exception as e:
         return str(e)
