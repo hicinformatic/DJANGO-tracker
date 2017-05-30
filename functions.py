e3634168a44abc4ef89da5faf117023218b3743d
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-30 20:09:09 +0200

    test

--- functions.py
+++ functions.py
@@ -241,7 +241,7 @@ def addVisitors(contenttype, task, script):
             domains = []
             datas = json.load(json_data)
             for domain in datas['domains']:
-                domains.append(uuid.UUID('domain'))
+                domains.append(uuid.UUID(domain))
             domains = Domain.objects.filter(id__in=domains)
             with open("log.json", 'w') as outfile:
                 json.dump(domains, outfile, indent=4)
