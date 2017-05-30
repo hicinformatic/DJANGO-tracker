98b2785ebe7080cb83d6397e4f26cff401b3b456
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2017-05-30 20:27:31 +0200

    test

--- models.py
+++ models.py
@@ -53,7 +53,7 @@ class Domain(models.Model):
         verbose_name_plural = _('Domains tracked')
 
     def __str__(self):
-        return self.id
+        return self.domain
 
     def save(self, *args, **kwargs):
         self.javascript = conf['example'].format(reverse('tracker:trackerJS', args=(self.id.hex,)), reverse('tracker:trackerSVG', args=(self.id.hex,)),)
